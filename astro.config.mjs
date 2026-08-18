6eec020af8e0ac34d8e778bd27679d3fae627588
sudhirsharmabca06 <sudhir.sharmabca06@gmail.com>
2026-08-19 00:34:06 +0530

    change domain name
    change domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://drsiddhartharora.com',
+  site: 'https://draroraeyecare.com',
   output: 'static',
   build: {
     format: 'directory'
